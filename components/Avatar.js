b653fa6fa435c66307b661083b189a4d60987e43
leandro-cajal <leo_origin@hotmail.com>
2024-06-25 14:03:31 -0300

    first commit

---
+++ components/Avatar.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Avatar = () => {
+  return <div className="hidden xl:flex xl:max-w-none ">
+    <img 
+      className="ml-auto rounded-full translate-z-0 w-full h-full" 
+      src={'/avatar-2.png'} 
+      alt="" />
+  </div>;
+};
+
+export default Avatar;
