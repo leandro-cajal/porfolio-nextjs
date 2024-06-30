42c83f3ca243ca28f286d471027350a3f15e766b
leandro-cajal <leo_origin@hotmail.com>
2024-06-30 06:05:21 -0300

    working on work section

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -5,8 +5,8 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
-
 
 // data
 const serviceData = [
@@ -37,8 +37,60 @@ const serviceData = [
   },
 ];
 
+// import swiper react components
+import { Swiper, SwiperSlide} from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules
+import { FreeMode, Pagination} from 'swiper';
+
+
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15
+        },
+        640:{
+          slidesPerView: 3,
+          spaceBetween: 15
+        },
+      }}
+      freeMode={true}
+      pagination={{clickable:true}}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+      >
+        {
+          serviceData.map((item,index)=>{
+            return(
+              <SwiperSlide key={index}>
+                <section className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+                  {/* icon */}
+                  <div className="text-4xl text-accent mb-4">{item.icon}</div>
+                  {/* title & desc */}
+                  <article className="mb8
+                  ">
+                    <h6 className="mb-2 text-lg">{item.title}</h6>
+                    <p className="max-w-[350px] leading-normal">item.description</p>
+                  </article>
+                  {/* arrow */}
+                  <div className="text-3xl">
+                    <RxArrowTopRight  className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300"/>
+                  </div>
+                </section>
+              </SwiperSlide>
+            )
+          })
+        }
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- pages/about/index.js
+++ pages/about/index.js
@@ -117,35 +117,64 @@ const About = () => {
         animate="show"
         exit="hidden"
         className='hidden xl:flex absolute -bottom-[5%] -left-80'>
-        <div className='w-[550px]'>
+        <div className=' w-[550px]'>
           <Avatar />
         </div>
       </motion.div>
-      <div className='container mx-auto h-full w-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div className='flex-1 flex flex-col justify-center'>
-          <h2 className='h2'>
-            Captivating <span className='text-accent'>stories</span> birth magnificent desings.</h2>
-          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>10 years ago, I began freelancing as a developer</p>
+      <div className='container mx-auto h-full w-full flex flex-col z-20 items-center xl:flex-row gap-x-6'>
+        <div className='flex-1 flex flex-col justify-start'>
+          <motion.h2
+            variants={fadeIn('right', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='h2'>
+            Captivating <span className='text-accent'>stories</span> birth magnificent desings.</motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>2 years ago, I began freelancing as a developer</motion.p>
           {/* counters */}
-          <div className=''>
-            {/* experience */}
-            <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
-              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-                <CountUp start={0} end={250} duration={8} />
+          <motion.div 
+            variants={fadeIn('right', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='hidden md:flex md:max-x-xl xl:max-w-none mx-auto xl:mx-0 mb-8'>
+            <div className='flex flex-1 xl:gap-x-6'>
+              {/* experience */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={2} duration={8} /> +
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Years of experience</div>
               </div>
-              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Satisfied clients</div>
-            </div>
-            
-            <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
-              <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-                <CountUp start={0} end={10} duration={8} /> +
+              {/* projects */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={12} duration={4} />
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Finished projects</div>
               </div>
-              <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Years of experience</div>
+              {/* clients */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={6} duration={6} />
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Satisfied clients</div>
+              </div>
             </div>
-          </div>
+          </motion.div>
         </div>
         {/* info */}
-        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+        <motion.div 
+          variants={fadeIn('left', 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 z-20 relative'>
             {aboutData.map((item, itemIndex) => (
               <div
@@ -169,15 +198,16 @@ const About = () => {
                     {/* icons */}
                     {item.icons?.map((icon, iconIndex) => {
                       return (
-                      <div key={iconIndex} className='text-2xl'>{icon}</div>
-                      )}
+                        <div key={iconIndex} className='text-2xl'>{icon}</div>
+                      )
+                    }
                     )}
                   </div>
                 </div>
               )
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
