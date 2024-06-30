864cc6433a0ab956c9693dca84f3e2146ac36495
leandro-cajal <leo_origin@hotmail.com>
2024-06-30 13:08:41 -0300

    working on info details

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/services': { page: '/services' },
+      '/testimonials': { page: '/testimonials' },
+      '/work': { page: '/work' },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
