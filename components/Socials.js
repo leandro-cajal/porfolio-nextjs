b689086ddf94501a60a5b8548d045a9fb439fac3
leandro-cajal <leo_origin@hotmail.com>
2024-07-28 07:36:43 -0300

    adding spanish version

--- components/Socials.js
+++ components/Socials.js
@@ -23,15 +23,17 @@ const Socials = () => {
     setIsDropdownVisible(false);
   };
 
+  const resumeLink = (selectedLanguage === "español" ? "/leandro-cajal-cv.pdf" : "/leandro-cajal-cv-en.pdf")
+
   return (
     <div className="flex items-center gap-x-5 text-lg">
       <Link target="_blank" href={'https://www.linkedin.com/in/leandro-javier-cajal-996899128/'} className="hover:text-accent text-xl transition-all duration-300" title="Linkedin">
         <RiLinkedinLine />
       </Link>
-      <Link target="_blank" href={'https://github.com/leandro-cajal'} className="hover:text-accent text-xl transition-all duration-300" title="Github">
+      <Link target="_blank" href='https://github.com/leandro-cajal' className="hover:text-accent text-xl transition-all duration-300" title="Github">
         <RiGithubLine />
       </Link>
-      <Link href="/leandro-cajal-cv-en.pdf" target="_blank" className="hover:text-accent transition-all duration-300" title="CV">
+      <Link href={resumeLink} target="_blank" className="hover:text-accent transition-all duration-300" title="CV">
         <FaFileAlt />
       </Link>
       <div className="relative">
