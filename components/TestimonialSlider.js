96ea92a32e949a4864f9bc2634caaaf0afecb79c
leandro-cajal <leo_origin@hotmail.com>
2024-07-28 08:46:15 -0300

    adding spanish texts

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -23,11 +23,6 @@ const testimonialSlider = [
   },
 ];
 
-import dynamic from 'next/dynamic';
-
-const Swiper = dynamic(() => import('swiper/react').then(mod => mod.Swiper), { ssr: false });
-const SwiperSlide = dynamic(() => import('swiper/react').then(mod => mod.SwiperSlide), { ssr: false });
-
 // import swiper react components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
